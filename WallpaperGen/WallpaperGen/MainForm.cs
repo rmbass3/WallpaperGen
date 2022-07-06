9b6a0174bf3c9f574d7251f4de7015738af20fc9
Ryan <rmbass3@gmail.com>
2022-07-05 22:27:12 -0400

    Added API call

---
+++ WallpaperGen/WallpaperGen/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Net.Http.Formatting;
+
+namespace WallpaperGen
+{
+    public partial class MainForm : Form
+    {
+        private const string URL = "https://wallhaven.cc/api/v1/search";
+        private string urlParameters = "?q=nature";
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void getWallpapersButton_Click(object sender, EventArgs e)
+        {
+            AllocConsole();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // List data response.
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body.
+                var dataObjects = response.Content.ReadAsAsync<DataObject>();  //Make sure to add a reference to System.Net.Http.Formatting.dll
+            }
+            else
+            {
+                Console.WriteLine("doesn't work");
+            }
+
+            // Make any other calls using HttpClient here.
+
+            // Dispose once all HttpClient calls are complete. This is not necessary if the containing object will be disposed of; for example in this case the HttpClient instance will be disposed automatically when the application terminates so the following call is superfluous.
+            client.Dispose();
+        }
+
+
+    }
+}
