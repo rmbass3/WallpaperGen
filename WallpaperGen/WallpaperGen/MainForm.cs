75b30348672dccb51c61e5945af967ccc62a79e3
Ryan <rmbass3@gmail.com>
2022-07-06 01:12:56 -0400

    Added Wallpaper class

--- WallpaperGen/WallpaperGen/MainForm.cs
+++ WallpaperGen/WallpaperGen/MainForm.cs
@@ -11,41 +11,64 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Net.Http.Formatting;
+using Newtonsoft.Json.Linq;
 
 namespace WallpaperGen
 {
     public partial class MainForm : Form
     {
-        private const string URL = "https://wallhaven.cc/api/v1/search";
-        private string urlParameters = "?q=nature";
         [System.Runtime.InteropServices.DllImport("kernel32.dll")]
         private static extern bool AllocConsole();
+
+        private const string URL = "https://wallhaven.cc/api/v1/search";
+        private string urlParameters = "?q=nature";
+        private List<Wallpaper> wallpaperList = new List<Wallpaper>();
+
 
         public MainForm()
         {
             InitializeComponent();
         }
 
-        private void getWallpapersButton_Click(object sender, EventArgs e)
+        private async void getWallpapersButton_Click(object sender, EventArgs e)
         {
             AllocConsole();
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri(URL);
 
             // Add an Accept header for JSON format.
-            client.DefaultRequestHeaders.Accept.Add(
-            new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
             // List data response.
             HttpResponseMessage response = client.GetAsync(urlParameters).Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
             if (response.IsSuccessStatusCode)
             {
                 // Parse the response body.
-                var dataObjects = response.Content.ReadAsAsync<DataObject>();  //Make sure to add a reference to System.Net.Http.Formatting.dll
+                string resultString = response.Content.ReadAsStringAsync().Result;
+                JObject json = JObject.Parse(resultString);
+                //Console.WriteLine(json["data"].First().ToString());
+
+                foreach (JObject wp in json["data"])
+                {
+                    wallpaperList.Add(createWallpaper(wp));
+                }
+
+                foreach (Wallpaper wp in wallpaperList)
+                {
+                    Console.WriteLine("~~~~~~~");
+                    Console.WriteLine(wp.id);
+                    Console.WriteLine(wp.url);
+                    foreach (string thumb in wp.thumbs)
+                    {
+                        Console.WriteLine(thumb);
+                    }
+                    Console.WriteLine(wp.resolution);
+                    
+                }
             }
             else
             {
-                Console.WriteLine("doesn't work");
+                Console.WriteLine("Error");
             }
 
             // Make any other calls using HttpClient here.
@@ -54,6 +77,23 @@ namespace WallpaperGen
             client.Dispose();
         }
 
+        private Wallpaper createWallpaper(JObject wp)
+        {
+            if (wp != null)
+            {
+                string[] thumbArr = new string[3];
+                thumbArr[0] = (string)wp["thumbs"]["large"];
+                thumbArr[1] = (string)wp["thumbs"]["original"];
+                thumbArr[2] = (string)wp["thumbs"]["small"];
+
+                Wallpaper wallpaper = new Wallpaper(wp["id"].ToString(), wp["path"].ToString(), thumbArr, wp["resolution"].ToString());
+                return wallpaper;
+            }
+            return null;
+
+        }
 
     }
+
+    
 }
---
+++ WallpaperGen/WallpaperGen/Wallpaper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace WallpaperGen
+{
+    public class Wallpaper
+    {
+        public string id { get; set; }
+        public string url { get; set; }
+        public string[] thumbs { get; set; }
+        public string resolution { get; set; }
+
+        public Wallpaper(string id, string url, string[] thumbs, string resolution)
+        {
+            this.id = id;
+            this.url = url;
+            this.thumbs = thumbs;
+            this.resolution = resolution;
+        }
+    }
+}
