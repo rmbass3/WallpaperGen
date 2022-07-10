6fc8dfb624ec86653f4d30e8ab06b1c0b86b1426
Ryan <rmbass3@gmail.com>
2022-07-10 16:31:21 -0400

    Created wallpaper switcher

---
+++ WallpaperSwitcher/WallpaperSwitcher/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace WallpaperSwitcher
+{
+    public class Program
+    {
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern Int32 SystemParametersInfo(UInt32 action, UInt32 uParam, String vParam, UInt32 winIni);
+
+        private static readonly UInt32 SPI_SETDESKWALLPAPER = 0x14;
+        private static readonly UInt32 SPIF_UPDATEINIFILE = 0x01;
+        private static readonly UInt32 SPIF_SENDWININICHANGE = 0x02;
+
+        static void Main()
+        {
+            setWallpaper(@"C:/Users/rmbas/OneDrive/Desktop/wallpaper2.jpg");
+        }
+
+        public static void setWallpaper(string path)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            key.SetValue(@"WallpaperStyle", 0.ToString()); // 2 is stretched
+            key.SetValue(@"TileWallpaper", 0.ToString());
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+}
