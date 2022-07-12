e61eaf66610a0ac46765478833a30104e8e9f653
Ryan <rmbass3@gmail.com>
2022-07-12 02:09:11 -0400

    Wallpaper switcher functioning

--- WallpaperSwitcher/WallpaperSwitcher/Program.cs
+++ WallpaperSwitcher/WallpaperSwitcher/Program.cs
@@ -6,6 +6,8 @@ namespace WallpaperSwitcher
 {
     public class Program
     {
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
 
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         private static extern Int32 SystemParametersInfo(UInt32 action, UInt32 uParam, String vParam, UInt32 winIni);
@@ -16,15 +18,19 @@ namespace WallpaperSwitcher
 
         static void Main(string[] args)
         {
+            //AllocConsole();
+            if (args.Length == 0) return;
+            string cWall = getCurrentWallpaper();
+            
+            string match = findMatchingWallpaper(args, cWall);
 
-            string[] paths;
-
-            if (args.Length > 0)
+            string wp = selectWallpaper(args, match);
+            if (wp != null)
             {
-                paths = args[0].Split(' ');
-                setWallpaper(@paths[0]);
+                setWallpaper(@wp);
             }
-
+            
+            //Console.ReadLine();
         }
 
         public static void setWallpaper(string path)
@@ -34,6 +40,64 @@ namespace WallpaperSwitcher
             key.SetValue(@"TileWallpaper", 0.ToString());
 
             SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+
+        public static string getCurrentWallpaper()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop"))
+            {
+                if (key != null)
+                {
+                    Object o = key.GetValue("WallPaper");
+                    if (o != null)
+                    {
+                        return o.ToString();
+                    }
+                }
+            }
+            return null;
+        }
+            
+
+        public static string findMatchingWallpaper(string[] args, string cWall)
+        {
+            //AllocConsole();
+            //Console.WriteLine("cWall: " + cWall);
+            foreach (string arg in args)
+            {
+                //Console.WriteLine("arg: " + arg);
+                if (arg == cWall)
+                {
+                    //Console.WriteLine("true");
+                    return arg;
+                }
+            }
+            return null;
+        }
+
+        public static string selectWallpaper(string[] args, string match)
+        {
+            /*
+            AllocConsole();
+
+            Console.WriteLine("match: " + match);
+
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            */
+
+            if (match != null)
+            {
+                args = Array.FindAll(args, arg => arg != match).ToArray();
+            }
+
+            Random rand = new Random();
+
+            int i = rand.Next(args.Length);
+
+            return args[i];
         }
     }
 }
